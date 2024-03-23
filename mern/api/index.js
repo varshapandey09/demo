ff186928b251dd18232bde70fc74c72aaff2942a
unknown <varsha280902>
2024-03-24 00:34:49 +0530

    error handling,middleware etc

--- mern/api/index.js
+++ mern/api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log(err);
 }); 
 const app= express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
@@ -18,4 +20,14 @@ app.listen(3000, () => {
     }
 );
 
-app.use("/api/user", userRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use((err, req, res, next) => {
+    const statusCode =err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
