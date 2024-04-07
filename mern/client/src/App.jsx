c23893afc706d8b42edf1713afe4b9f0e901eb03
Khushi Gunwant <vartikagunwant2004@gmail.com>
2024-04-07 10:28:54 +0530

    sign up completed

--- mern/client/src/App.jsx
+++ mern/client/src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes , Route } from 'react-router-dom';
 import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Home from './pages/Home';
 import Header from './components/Header';
+import SignUp from './pages/SignUp';
 
 export default function App() {
     return <BrowserRouter>
